7b88431ed4a77edbab7b40f20cd5dad8a272364d
Arslan Razzaq <arslan6591@gmail.com>
2026-02-02 14:21:37 +0500

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,8 @@
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes.ts";
-import { serveStatic } from "./static.ts";
+import { registerRoutes } from "./routes";
+import { serveStatic } from "./static";
 import { createServer } from "http";
-import { storage } from "./storage.ts";
+import { storage } from "./storage";
 import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
